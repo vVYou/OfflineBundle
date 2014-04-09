b42f058099d84a8f2968fe89dc3f2a3ee8b6402d
Van Ouytsel V <vincent.vanouytsel@gmail.com>
2014-04-09 14:43:45 +0200

    update des dates de modifs mais le probleme du listener persiste

--- Manager/LoadingManager.php
+++ Manager/LoadingManager.php
@@ -508,7 +508,7 @@ class LoadingManager
         echo 'ModificationDate of XML Before : '.$modification_date->format('d/m/Y H:i:s').'<br/>';
         
         // Update of the creation and modification date of the resource. 
-       // $this->changeDate($newResourceNode,$creation_date,$modification_date);
+        $this->changeDate($newResourceNode,$creation_date,$modification_date);
 
     }
     

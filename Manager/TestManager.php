94baf097d820653d46d694171641bb1800fb241e
Van Ouytsel V <vincent.vanouytsel@gmail.com>
2014-08-01 16:52:59 +0200

    Test Taille du packet

--- Manager/TestManager.php
+++ Manager/TestManager.php
@@ -44,36 +44,48 @@ class TestManager
 
     public function testTransfer($user)
     {
-        $fichier1M = "";
-        $fichier5M = "";
-        $fichier10M = "";
-        $fichier50M = "";
+        $zipArray = array(
+        'fichier1M' => SyncConstant::SYNCHRO_DOWN_DIR.$user->getId()."/sync_474A1C5C-B54B-4DCF-AB46-B8AB47CBB2B0.zip",
+        'fichier5M' =>  SyncConstant::SYNCHRO_DOWN_DIR.$user->getId()."/sync_F9024E90-9C91-4B73-94CE-1F2A8317495F.zip",
+        'fichier10M' =>  SyncConstant::SYNCHRO_DOWN_DIR.$user->getId()."/sync_3F28AD04-58DC-458C-B09E-4A19E630588D.zip",
+        'fichier25M' =>  SyncConstant::SYNCHRO_DOWN_DIR.$user->getId()."/sync_C179B49F-5183-4D96-A13A-97816E97EA16.zip",
+        'fichier50M' =>  SyncConstant::SYNCHRO_DOWN_DIR.$user->getId()."/sync_9FEAF80B-DB57-4737-9CCB-DCE01871608F.zip");
         
         echo "DEBUT DU TEST<br/>";
         $rFile = fopen("test_transfer.txt", "w");
         fwrite($rFile, "DEBUT FICHIER TEST TAILLE PACKET\n");
+        fwrite($rFile, "Taille des fragments : ".SyncConstant::MAX_PACKET_SIZE);
+        foreach($zipArray as $file){
+            echo "DOING ".$file."   ----------------<br/>";
+            $this->testUploadFile($rFile, $file, $user);
+        }
         fclose($rFile);
         echo "FIN DU TEST <br/>";
     }
     
     private function testUploadFile($report, $file, $user)
     {
+        ini_set('max_execution_time', 0);
         fwrite($report, "\n------------------------------\n");
         fwrite($report, "TEST envoi fichier : ".$file."\n");
+        fwrite($report, "Taille du fichier : ".filesize($file)."\n");
         for($i = 0; $i < 20; $i++)
         {
             $begin = new Datetime;
             $this->transferManager->uploadArchive($file, $user, 0);
             $stop = new Datetime;
-            fwrite($report, $stop-$begin.",");
+            fwrite($report, print_r($stop->diff($begin)).",");
+            echo "      exe ".$i."    TEMPS : ".print_r($stop->diff($begin))."<br/>";
         }
         fwrite($report, "END TEST\n");
     }
     
     private function testDownloadFile($report, $file, $user)
     {
+        ini_set('max_execution_time', 0);
         fwrite($report, "\n------------------------------\n");
         fwrite($report, "TEST telechargement fichier : ".$file."\n");
+        fwrite($report, "Taille du fichier : ".filesize($file)."\n");
         $totalFrag = $this->transferManager->getNumberOfFragmentsOnline($file, $user);
         for($i = 0; $i < 20; $i++)
         {

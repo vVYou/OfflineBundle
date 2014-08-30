5f89afd1c543c43516964f5f49a943c3b28961db
pyjoe <pierre-yves.legena@student.uclouvain.be>
2014-08-30 17:56:21 +0200

    Update OfflineFile.php

--- Model/Resource/OfflineFile.php
+++ Model/Resource/OfflineFile.php
@@ -111,7 +111,6 @@ class OfflineFile extends OfflineResource
         $modificationDate->setTimestamp($resource->getAttribute('modification_date'));
 
         $type = $this->resourceManager->getResourceTypeByName($resource->getAttribute('type'));
-        // $creator = $this->userRepo->findOneBy(array('exchangeToken' => $resource->getAttribute('creator')));
         $creator = $this->getCreator($resource);
         $parentNode = $this->resourceNodeRepo->findOneBy(array('hashName' => $resource->getAttribute('hashname_parent')));
 
@@ -198,7 +197,7 @@ class OfflineFile extends OfflineResource
         $modificationDate->setTimestamp($resource->getAttribute('modification_date'));
 
         $type = $this->resourceManager->getResourceTypeByName($resource->getAttribute('type'));
-        $creator = $this->userRepo->findOneBy(array('exchangeToken' => $resource->getAttribute('creator')));
+        $creator = $this->getCreator($resource);
         $parentNode = $this->resourceNodeRepo->findOneBy(array('hashName' => $resource->getAttribute('hashname_parent')));
 
         if (count($parentNode) < 1) {
